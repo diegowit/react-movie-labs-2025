c0b19c6c5d734c94c1be0e66faecf060d2470caa
Diego <20098757@mail.wit.ie>
2025-10-20 14:54:14 +0100

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -3,7 +3,7 @@ import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import AddToPlaylistsIcon from '../components/cardIcons/PlaylistAdd'
 import { getUpcomingMovies } from "../api/tmdb-api";
 
 
@@ -34,7 +34,7 @@ return (
         title="Upcoming Movies"
         movies={movies}
         action={(movie) => {
-          return <AddToFavoritesIcon movie={movie} />
+          return <AddToPlaylistsIcon movie={movie} />
         }}
       />
   );
