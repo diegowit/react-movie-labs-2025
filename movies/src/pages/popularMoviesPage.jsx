8c9e7c4c4328f1623b50007e787fba3f84e1749f
Diego <20098757@mail.wit.ie>
2025-11-02 23:12:16 +0000

    Popular-Movies-Page-added

---
+++ movies/src/pages/popularMoviesPage.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { getMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from '@tanstack/react-query';
+import Spinner from '../components/spinner';
+import AddToPlaylistsIcon from '../components/cardIcons/PlaylistAdd'
+import { getPopularMovies } from "../api/tmdb-api";
+
+
+const PopularMoviesPage = (props) => {
+
+  const { data, error, isPending, isError  } = useQuery({
+    queryKey: ['popular'],
+    queryFn: getPopularMovies,
+  })
+  
+  if (isPending) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+  const addToFavorites = (movieId) => true 
+
+return (
+      <PageTemplate
+        title="Popular Movies"
+        movies={movies}
+
+        action={(movie) => {
+          return <AddToPlaylistsIcon movie={movie} />
+        }}
+      />
+
+  );
+};
+export default  PopularMoviesPage;
+
+
+
